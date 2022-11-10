a36dbbf33bc36712ac9cbac3236291cc45ab59b9
Rudra <srudra3110@gmail.com>
2022-11-10 23:00:37 +0530

    info changed of about

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -6,17 +6,17 @@ const Info = () => {
             <div className="about__box">
                 <i class="bx bx-award about__icon"></i>
                 <h3 className="about__title">Experience</h3>
-                <span className="about__subtitle">1 Year of class</span>
+                <span className="about__subtitle">2 year +</span>
             </div>
             <div className="about__box">
                 <i class="bx bx-briefcase-alt about__icon"></i>
                 <h3 className="about__title">Completed</h3>
-                <span className="about__subtitle">20+ Projects</span>
+                <span className="about__subtitle">15+ Projects</span>
             </div>
             <div className="about__box">
                 <i class="bx bx-support about__icon"></i>
                 <h3 className="about__title">Support</h3>
-                <span className="about__subtitle">Online 24/7</span>
+                <span className="about__subtitle">Online</span>
             </div>
         </div>
     )
